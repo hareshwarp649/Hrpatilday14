4e6c487b44e805e21317f861b7af08f3c81a187f
Harry <hareshwarp649>
2022-03-06 10:29:05 +0530

    Peak Pop from The Stack

---
+++ Stack2/NodeStack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrucStack1
+{
+    internal class NodeStack
+    {
+
+        internal int data;
+        internal NodeStack next;
+
+
+        public NodeStack(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+}
---
+++ Stack2/StackSecond.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrucStack1
+{
+    internal class StackSecond
+    {
+        NodeStack top;
+        public StackSecond()
+        {
+            this.top = null;
+        }
+
+        internal void Push(int value)
+        {
+            NodeStack newNode = new NodeStack(value);
+            if (top == null)
+            {
+                newNode.next = null;
+            }
+            else
+            {
+                newNode.next = top;
+            }
+            top = newNode;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+
+        internal void Pop()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack Underflow. Deletion not possible");
+                return;
+            }
+
+            Console.WriteLine("Item popped is {0}", top.data);
+            top = top.next;
+        }
+
+        internal void Peek()
+        {
+            Console.WriteLine("{0} is on the top of Stack", this.top.data);
+        }
+
+        internal void PrintStack()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return;
+            }
+
+            NodeStack temp = this.top;
+
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
