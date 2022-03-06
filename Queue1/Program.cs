13216abcc30c3f471091762b71bd23784339b885
Harry <hareshwarp649>
2022-03-06 10:44:55 +0530

    create a Queue

---
+++ Queue1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            QueueRef();
+            Console.ReadLine();
+        }
+
+        public static void QueueRef()
+        {
+            Queuefirst fir = new Queuefirst(100);
+            QueueSer ser = new QueueSer();
+
+            ser.EnQueue(fir, 18);
+            ser.EnQueue(fir, 19);
+            ser.EnQueue(fir, 48);
+            ser.EnQueue(fir, 56);
+
+            ser.FrontElement(fir);
+            ser.RearElement(fir);
+
+            ser.PrintQueue(fir);
+
+            ser.DeQueue(fir);
+
+            ser.FrontElement(fir);
+            ser.RearElement(fir);
+            ser.PrintQueue(fir);
+        }
+    }
+}
---
+++ Queue1/Queuefirst.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructQueue
+{
+    internal class Queuefirst
+    {
+        
+        internal int front;
+        internal int rear;
+        internal int size;
+
+        internal int capacity;
+
+        internal int[] array;
+
+        public Queuefirst(int capacity)
+        {
+            this.capacity = capacity;
+            front = this.size = 0;
+            rear = capacity - 1;
+            array = new int[capacity];
+        }
+    }
+
+    internal class QueueSer
+    {
+        internal bool IsFull(Queuefirst queue)
+        {
+            return (queue.size == queue.capacity);
+        }
+
+       
+        internal bool IsEmpty(Queuefirst queue)
+        {
+            return (queue.size == 0);
+        }
+
+       
+        internal void EnQueue(Queuefirst queue, int item)
+        {
+            if (IsFull(queue))
+            {
+                return;
+            }
+            queue.rear = (queue.rear + 1) % queue.capacity;
+            queue.array[queue.rear] = item;
+            queue.size = queue.size + 1;
+            Console.WriteLine("Value {0} has been Enqueued", item);
+        }
+
+       
+        internal void DeQueue(Queuefirst queue)
+        {
+            if (IsEmpty(queue))
+            {
+                Console.WriteLine("The Queue is empty");
+                return;
+            }
+
+            int item = queue.array[queue.front];
+            queue.front = (queue.front + 1) % queue.capacity;
+            queue.size = queue.size - 1;
+            Console.WriteLine("Item deleted is {0}", item);
+        }
+
+        
+        internal void FrontElement(Queuefirst queue)
+        {
+            if (IsEmpty(queue))
+            {
+                Console.WriteLine("The Queue is empty");
+                return;
+            }
+            Console.WriteLine("Front : {0}", queue.array[queue.front]);
+        }
+
+      
+        internal void RearElement(Queuefirst queue)
+        {
+            if (IsEmpty(queue))
+            {
+                Console.WriteLine("The Queue is empty");
+                return;
+            }
+            Console.WriteLine("Rear : {0}", queue.array[queue.rear]);
+        }
+
+        internal void PrintQueue(Queuefirst queue)
+        {
+            for (int i = queue.front; i <= queue.rear; i++)
+            {
+                Console.Write(queue.array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
